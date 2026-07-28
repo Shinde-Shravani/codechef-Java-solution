dad6945c08d5640c2324aa0d7632a2fcbd2a0325
Shravani Shinde <shinde.shravani39@gmail.com>
2026-07-29 00:16:18 +0530

    Create solution.java

---
+++ medium/Brick-Comparisions/solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T > 0) {
+            int N = sc.nextInt();
+
+            int max = sc.nextInt();
+            int index = 1;
+
+            for (int i = 2; i <= N; i++) {
+                int current = sc.nextInt();
+
+                if (current > max) {
+                    max = current;
+                    index = i;
+                }
+            }
+
+            System.out.println(index);
+        }
+    }
+}
