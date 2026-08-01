fdb4ed759cf33c8512a39fcedca24a2ff2d5d2bc
Shravani Shinde <shinde.shravani39@gmail.com>
2026-08-01 22:14:25 +0530

    Add solution.java for calculating currency notes

---
+++ easy/2000/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int N = sc.nextInt();
+	    int notes=N*2000;
+	    int rs=notes/500;
+	    System.out.println(rs);
+
+	}
+}
