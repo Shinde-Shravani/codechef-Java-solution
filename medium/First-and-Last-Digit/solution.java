ba44d15d537b06ad48bf6cf9d4464bfc220d8c5b
Shravani Shinde <shinde.shravani39@gmail.com>
2026-08-05 23:44:37 +0530

    Create solution.java

---
+++ medium/First-and-Last-Digit/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+
+            int lastDigit = N % 10;
+
+            while (N >= 10) {
+                N = N / 10;
+            }
+
+            int firstDigit = N;
+
+            System.out.println(firstDigit + lastDigit);
+        }
+    }
+}
