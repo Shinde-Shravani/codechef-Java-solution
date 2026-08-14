05d91bb0a5d2019dc75ce59025616fd1c4805b70
Shravani Shinde <shinde.shravani39@gmail.com>
2026-08-14 18:41:13 +0530

    Add solution for Delivery Fee problem in Java

---
+++ easy/Delivery-Fee/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		int K=sc.nextInt();
+		if(A>=K){
+		    System.out.println(A);
+		}else{
+		    System.out.println(A+B);
+		}
+
+	}
+}
